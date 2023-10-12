c81ba8e6fb90fedc5126a0e2aefdd882506ac62e
ohataken <11092425+ohataken@users.noreply.github.com>
2023-10-13 00:50:12 +0900

    output file extension

--- src/main/java/org/openapitools/codegen/CalvinGenerator.java
+++ src/main/java/org/openapitools/codegen/CalvinGenerator.java
@@ -81,7 +81,7 @@ public class CalvinGenerator extends DefaultCodegen implements CodegenConfig {
      */
     modelTemplateFiles.put(
       "model.mustache", // the template to use
-      ".sample");       // the extension for each file to write
+      ".java");       // the extension for each file to write
 
     /**
      * Api classes.  You can write classes for each Api file with the apiTemplateFiles map.
